fc1ac8487316bb005724f74416eaa522d6cc77fc
GarrimGyr <69116070+GarrimGyr@users.noreply.github.com>
2022-08-11 18:23:32 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {table, minifyRecords} from "./api/utils/airtable.js"
-import Item from "/Users/JDevine/Desktop/zoning_tracker_draft/zoning_tracker/components/item.js";
+import Item from "/components/item.js";
 
 export default function Home({initialItems}) {
   console.log(initialItems);
